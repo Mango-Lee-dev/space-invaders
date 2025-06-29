5a7f69c4fb93467083f8ca2320afa00f13f093d5
wootaiklee <fined000@naver.com>
2025-06-29 13:04:03 +0900

    first commit

---
+++ image.py
@@ -0,0 +1,26 @@
+import pygame
+
+print("startup");
+
+pygame.init()
+pygame.key.set_repeat(500, 500)
+surface = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+
+image = pygame.image.load("assets/images/fighter.png")
+scale_up_image = pygame.transform.scale(image, (100, 100))
+
+while True:
+  print("Update") 
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      print("Shutdown")
+      pygame.quit()
+      exit()
+      break
+
+  print("Render")
+  surface.fill((0, 0, 0))
+  surface.blit(scale_up_image, (100, 200))
+  pygame.display.update()
+  clock.tick(30)
