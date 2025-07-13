cb28d1de6a6ccbaa7733e24c340efae489173255
wootaiklee <fined000@naver.com>
2025-07-13 22:08:56 +0900

    refactor: alien, fighter class 분리

--- image.py
+++ image.py
@@ -1,4 +1,6 @@
 import pygame
+from game_objects.fighter import Fighter
+from game_objects.alien import Alien
 
 print("startup");
 
@@ -7,35 +9,15 @@ pygame.key.set_repeat(500, 500)
 surface = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
-image = pygame.image.load("assets/images/fighter.png")
-scale_up_image = pygame.transform.scale(image, (image.get_width() * 2, image.get_height() * 2))
+fighter = Fighter()
 
-x = 640 / 2 - scale_up_image.get_width() / 2
-y = 480 - scale_up_image.get_height() - 10
-
-alien_image = pygame.image.load("assets/images/alien1.png")
-scale_up_alien_image = pygame.transform.scale(alien_image, (alien_image.get_width() * 2, alien_image.get_height() * 2))
-
-alien_x = 70
-alien_y = 100
-
-alien_pos = [
-  (70, 100),
-  (100, 100),
-  (130, 100),
-  (160, 100),
-  (190, 100),
-  (220, 100),
-  (250, 100),
-  (280, 100),
-  (310, 100),
-  (340, 100),
-  (370, 100),
-  (400, 100),
-  (430, 100),
-  (460, 100),
-  (490, 100),
-]
+aliens = []
+for y in range(5):
+  for x in range(12):
+    alien = Alien()
+    aliens.append(alien)
+    alien.x = 70 + 50 * x
+    alien.y = 100 + 70 * y
 
 while True:
   print("Update") 
@@ -48,8 +30,9 @@ while True:
 
   print("Render")
   surface.fill((0, 0, 0))
-  surface.blit(scale_up_image, (x, y))
-  for pos in alien_pos:
-    surface.blit(scale_up_alien_image, pos)
+  fighter.draw(surface)
+
+  for alien in aliens:
+    alien.draw(surface)
   pygame.display.update()
   clock.tick(30)
